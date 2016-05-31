0b6c06b32212c542f8e02f010b1cd2fa3a6c0e5c
Ansonparkour <Sean@Seans-Mac.local>
2016-05-30 21:22:45 -0700

    Week3

---
+++ RProgramming/R_Week3.R
@@ -0,0 +1,28 @@
+#cache matrx function 
+makeCacheMatrix <- function(x = matrix()) {
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    #get x from function input x 
+    get <- function() x
+    setinverse <- function(inverse) m <<- inverse
+    getinverse <- function() m
+    list(set = set, get = get,
+         setinverse = setinverse,
+         getinverse = getinverse)
+}
+
+#inverse function 
+cachinverse <- function(x, ...) {
+    m <- x$getinverse()
+    if(!is.null(m)) {
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinverse(m)
+    m
+}
